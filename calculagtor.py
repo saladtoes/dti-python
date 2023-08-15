b865f473b395d34ec5c34842f54cf70164c68c2a
Joel <joelocallaghan@hotmail.co.nz>
2023-08-16 09:11:42 +1200

    uploading

--- calculagtor.py
+++ calculagtor.py
@@ -1,8 +1,6 @@
 import easygui
-
 testtype = easygui.enterbox("What type of test is this?: ")
-outOf = easygui.integerbox("How many marks was the " + testtype + " out of?: ")
+outOf = easygui.integerbox("How many marks was the " + testtype + " test out of?: ")
 marksgotten = easygui.integerbox("How mant marks did you get?: ")
-percent = (marksgotten/outOf)*100
-easygui.integerbox("Your percentage for the " + testtype + "test is" + percent )
-
+percent = str(round((marksgotten/outOf)*100))
+easygui.msgbox("Your percentage for the " + testtype + "test is " + percent + "%" )
