422772b245f9348607334dd4621e12bae79e91df
saladtoes <joelocallaghan@hotmail.co.nz>
2023-08-22 14:01:29 +1200

    uploading

--- assessment.py
+++ assessment.py
@@ -1,7 +1,13 @@
 
 name = input("What is your name?: ")
-age = int(input("Hello " + name + " how old are you?: "))
     
+def contains_number(string):
+    return any(char.isdigit() for char in string)
+while contains_number(name):
+    print("Your name can not contain a number")
+    name = str(input("What is your name?: "))
+age = int(input("Hello ", name, " how old are you?: "))
+
 if age <= 7:
     print("youre too young to do this quiz")
 elif age >= 14:
@@ -13,7 +19,7 @@ global answers
 answers = ["a", "b", "c", "A", "B", "C"]
 question1Answers = [" A: Delete the message and try to forget about it\n", "B: Keep the text and show an adult you trust\n", "C: Text the person back and say something mean to them"]
 question2Answers = [" A: Tweet that they are an idiot and a loser\n", "B: Ask your friends to give the person a hard time\n", "C: Tell an adult you trust\n"]
-question3Answers = [" A: Nickname\n", "B: Your name\n", "C: Your email address\n"]
+question3Answers = [" A: Nicsukname\n", "B: Your name\n", "C: Your email address\n"]
 question4Answers = [" A: Your video is rubbish\n", "B: Man, this is awful! Stick to playing spport or something.\n", "C: Congrats on your first video! Let me know if you'd like any help editing for your next video\n"]
 question5Answers = [' A: “We shouldn’t be mean to them just because they’re mean to us."\n', 'B: “Yeah, totally. They’re evil and deserve it!”\n', 'C: “Yes, I think that is a great idea. Maybe they will understand what it feels like, and stop bullying us!”\n']
 questions = ["Someone sends you a text that is hurtful and makes you feel bad about yourself. What should you do?\n", "You find out that someone has posted an embarrassing picture of you online. What should you do?", "You want to join an online gaming site. Which of the following information is okay for you to post on the site.", "Someone in your class has posted their first video on YouTube and has asked you to comment on it. You don’t think the video is good because the editing is very choppy. What could you comment?", "Someone in your class is a real bully. Some of the other people in your class say: “Let’s get them back, and spam them with random texts.” What do you reply?"]
