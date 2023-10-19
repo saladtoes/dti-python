8bab7f18373139d39acc669fce7a5f3f2df8ddcf
Joel <joelocallaghan@hotmail.co.nz>
2023-10-19 13:45:36 +1300

    test

--- temperature.py
+++
@@ -1,6 +0,0 @@
-import easygui
-testtype = easygui.enterbox("What type of test is this?: ")
-totalmarks = easygui.integerbox("How many marks was the " + testtype + " test out of?: ")
-marksgotten = easygui.integerbox("How many marks did you get?: ")
-percentage = str(round((marksgotten/totalmarks)*100))
-easygui.msgbox("Your percentage for the " + testtype + " test is " + percentage + "%" )
